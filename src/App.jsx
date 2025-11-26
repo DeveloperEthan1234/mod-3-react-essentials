7bae0d0284d50c0dbf97d127286d963ad65a8241
ethan <developerethan@tutamail.com>
2025-11-25 20:31:47 -0500

    working with conditional rednering

--- src/App.jsx
+++ src/App.jsx
@@ -5,62 +5,67 @@ import TabButton from "./components/TabButton.jsx";
 import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState("");
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
   }
   return (
-    <div>
-      <Header />
-      <main>
-        <section id="core-concepts">
-          <h2>YOu look pretty</h2>
-          <ul>
-            {/* shorthand */}
-            <CoreConcepts {...CORE_CONCEPTS[0]} />
-            {/* longhand */}
-            <CoreConcepts
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcepts
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              image={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcepts
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image}
-            />
-          </ul>
-        </section>
+    <>
+      <div>
+        <Header />
+        <main>
+          <section id="core-concepts">
+            <h2>YOu look pretty</h2>
+            <ul>
+              {/* shorthand */}
+              <CoreConcepts {...CORE_CONCEPTS[0]} />
+              {/* longhand */}
+              <CoreConcepts
+                title={CORE_CONCEPTS[1].title}
+                description={CORE_CONCEPTS[1].description}
+                image={CORE_CONCEPTS[1].image}
+              />
+              <CoreConcepts
+                title={CORE_CONCEPTS[2].title}
+                description={CORE_CONCEPTS[2].description}
+                image={CORE_CONCEPTS[2].image}
+              />
+              <CoreConcepts
+                title={CORE_CONCEPTS[3].title}
+                description={CORE_CONCEPTS[3].description}
+                image={CORE_CONCEPTS[3].image}
+              />
+            </ul>
+          </section>
 
-        <section id="examples">
-          <h2>
-            <menu>
-              <TabButton onSelect={() => handleSelect("components")}>
-                Components
-              </TabButton>
-              <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
-              <TabButton onSelect={() => handleSelect("props")}>
-                Props
-              </TabButton>
-              <TabButton onSelect={() => handleSelect("state")}>
-                State
-              </TabButton>
-            </menu>
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>{EXAMPLES[selectedTopic].code}</pre>
-            </div>
-          </h2>
-        </section>
-      </main>
-    </div>
+          <section id="examples">
+            <h2>
+              <menu>
+                <TabButton onSelect={() => handleSelect("components")}>
+                  Components
+                </TabButton>
+                <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+                <TabButton onSelect={() => handleSelect("props")}>
+                  Props
+                </TabButton>
+                <TabButton onSelect={() => handleSelect("state")}>
+                  State
+                </TabButton>
+              </menu>
+              {!selectedTopic ? <p>Please select a topic</p> : null}
+              {selectedTopic ? (
+                <div id="tab-content">
+                  <h3>{EXAMPLES[selectedTopic].title}</h3>
+                  <p>{EXAMPLES[selectedTopic].description}</p>
+                  <pre>{EXAMPLES[selectedTopic].code}</pre>
+                </div>
+              ) : null}
+            </h2>
+          </section>
+        </main>
+      </div>
+    </>
   );
 }
 
